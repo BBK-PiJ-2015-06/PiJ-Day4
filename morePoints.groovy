e6e118bfc9bb53f287a4e2b4d9abb4ff8082a39f
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-12 11:14:10 +0100

    Code added to body of each method.

--- morePoints.groovy
+++ morePoints.groovy
@@ -4,20 +4,40 @@ class Point {
 	double y;
 	
 	double distanceTo(Point point) {
+		double dx = Math.abs(point.x - this.x)
+		double dy = Math.abs(point.y - this.y)
+		double distance = Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2));
+		return distance;
 	}
 
 	double distanceToOrigin() {
+		Point origin = new Point();
+		origin.x = 0;
+		origin.y = 0;
+		double distance = distanceTo(origin);
+		return distance;
 	}
 	
-	Point moveTo(double x, double y) {
+	void moveTo(double x, double y) {
+		this.x = x;
+		this.y = y;
 	}
 	
-	Point moveTo(Point point) {
+	void moveTo(Point point) {
+		this.x = point.x;
+		this.y = point.y;
 	}
 	
 	Point clone() {
+		Point copy = new Point();
+		copy.moveTo(this.x,this.y);
+		return copy;
 	}
 	
 	Point opposite() {
+		Point copy = new Point();
+		copy.moveTo(this.x * -1,this.y * -1);
+		return copy;
 	}
+	
 }
