5b3723ced25c4b04bf2cb5e9d8b12ec382cfdf7a
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-12 10:50:11 +0100

    First commit. Methods defined.

---
+++ morePoints.groovy
@@ -0,0 +1,23 @@
+class Point {
+
+	double x;
+	double y;
+	
+	double distanceTo(Point point) {
+	}
+
+	double distanceToOrigin() {
+	}
+	
+	Point moveTo(double x, double y) {
+	}
+	
+	Point moveTo(Point point) {
+	}
+	
+	Point clone() {
+	}
+	
+	Point opposite() {
+	}
+}
