d3610af361f62601cd5aeb90b4a7b182da4d7b71
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-12 11:56:51 +0100

    Bug within isOdd() method fixed. Used program provided by tutor to ensure methods have been written correctly.

--- integer.groovy
+++ integer.groovy
@@ -14,7 +14,7 @@ class Integer2 {
 		if (value%2 == 0) {
 			return true;
 		} else {
-			return flase;
+			return false;
 		}
 	}
 	
@@ -34,4 +34,22 @@ class Integer2 {
 		String str = value + "";
 		return str;
 	}
+}
+
+Integer2 i2 = new Integer2();
+print "Enter a number: ";
+String str = System.console().readLine();
+int i = Integer.parseInt(str);
+i2.setValue(i);
+print "The number you entered is "
+if (i2.isEven()) {
+	println "even.";
+} else if (i2.isOdd()) {
+	println "odd.";
+} else {
+	println "undefined!! Your code is buggy!";
+}
+int parsedInt = Integer.parseInt(i2.toString());
+if (parsedInt == i2.getValue()) {
+	println "Your toString() method seems to work fine.";
 }
