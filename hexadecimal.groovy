e8382fe8ba3be39c0c30ff8f178701f11e365e3f
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-13 11:54:14 +0100

    Solution completed. Methods tested and output is correct.

--- hexadecimal.groovy
+++ hexadecimal.groovy
@@ -1,3 +1,4 @@
+// Method for testing whether input is decimal or hexadecimal.
 void readNumType(String s){
 	if (s.length() > 2 && s.substring(0,2) == "0x") {
 		println "This is a hexadecimal number."
@@ -9,19 +10,72 @@ void readNumType(String s){
 	}
 }
 
+// Method for converting a hexadecimal number into a decimal number.
 int hexToDec(String s){
 	int output = 0;
-	for(int count = 0; count < s.length; count++) {
+	int index = 0;
+	s = s.substring(2);
+	for(int count = 0; count < s.length(); count++) {
 		char c = s.charAt(count);
-		output
-	
+		switch (c) {
+		case 'a':
+			index = 10;
+			break;
+		case 'b':
+			index = 11;
+			break;
+		case 'c':
+			index = 12;
+			break;
+		case 'd':
+			index = 13;
+			break;
+		case 'e':
+			index = 14;
+			break;
+		case 'f':
+			index = 15;
+			break;
+		default:
+			index = Character.getNumericValue(c);
+			break;
+		}
+		output = (index * Math.pow(16,s.length()-count-1)) + output;
 	}
+	return output;
 }
 
+// Method for converting a decimal number to a hexadecimal number.
 String decToHex(int i){
+	boolean finished = false;
+	String output = "";
+	int remainder = 0;
+	while(!finished) {
+		if(i%16 <= 9) {
+			remainder = i%16;
+			output = remainder + output;
+		} else if(i%16 == 10) {
+			output = "a" + output;
+		} else if(i%16 == 11) {
+			output = "b" + output;
+		} else if(i%16 == 12) {
+			output = "c" + output;	
+		} else if(i%16 == 13) {
+			output = "d" + output;
+		} else if(i%16 == 14) {
+			output = "e" + output;
+		} else {
+			output = "f" + output;
+		}
+		i = i/16;
+		if(i == 0) {
+			finished = true;
+		}
+	}
+	return "0x" + output;
 }
 
-
+// Test
 println "Please insert an integer in either decimal or hexadecimal format: ";
 String s = System.console().readLine();
 readNumType(s);
